e6fc4c455febe1b5c0fbdf69608b055863f85326
Proscka <proskinaviktoria861@gmail.com>
2024-06-27 19:24:40 +0300

    Add files via upload

---
+++ homework2.py/main.py
@@ -0,0 +1,13 @@
+problem="12"
+print(problem)
+hour="b 1.5"
+print(float)
+hoar=[1.5]
+print(hoar)
+name="Python"
+print(name)
+float="1.5//12"
+print(1.5//12)
+name="Курс_Python,problem,всего:12,hour,затраченно,часов:1.5,float,среднее время,выполнения:0.125 часа"
+print(name)
+
